a90e2aac456fa9b94cd7fbaee18c44eb21c28b55
supermads <madisonsfahey@gmail.com>
2021-02-22 14:33:50 -0800

    Stage 5 of Project

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,18 +1,14 @@
-def convert_conicoins():
-    # Amount of each currency equivalent to 1 conicoin
-    exchange_rates = {
-        "rub": 2.98,
-        "ars": 0.82,
-        "hnl": 0.17,
-        "aud": 1.9622,
-        "mad": 0.208
-    }
-    coins = float(input())
-    if coins.is_integer():
-        coins = int(coins)
-    for curr_type, rate in exchange_rates.items():
-        print(f"I will get {round((coins * rate),2)} {curr_type.upper()} from the sale of {coins} conicoins.")
+import requests
+import json
 
 
-convert_conicoins()
+def convert_currencies():
+    curr_choice = input().lower()
+    r = requests.get(f"http://www.floatrates.com/daily/{curr_choice}.json")
+    conversions = json.loads(r.text)
+    print(conversions["usd"])
+    print(conversions["eur"])
+
+
+convert_currencies()
 
