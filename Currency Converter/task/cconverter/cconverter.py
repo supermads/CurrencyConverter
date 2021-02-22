a51178a60b54e4ac6ae77646ab22e41ac1230856
supermads <madisonsfahey@gmail.com>
2021-02-22 13:34:21 -0800

    Stage 2 of Project

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,1 +1,10 @@
-print("Meet a conicoin!")
+def conicoins_to_dollars():
+    conicoins = int(input())
+    dollars = conicoins * 100
+    print(f"I have {conicoins} conicoins.")
+    print(f"{conicoins} conicoins cost {dollars} dollars.")
+    print("I am rich! Yippee!")
+
+
+conicoins_to_dollars()
+
