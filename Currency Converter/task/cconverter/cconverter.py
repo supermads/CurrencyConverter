8d50c2b65a3dcf8a240b9f541100020d4da2c83b
supermads <madisonsfahey@gmail.com>
2021-02-22 13:57:09 -0800

    Stage 4 of Project

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,9 +1,18 @@
-def conicoins_to_dollars():
-    conicoins = int(input("Please, enter the number of conicoins you have:\n"))
-    exchange_rate = float(input("Please, enter the exchange rate:\n"))
-    dollars = conicoins * exchange_rate
-    print(f"The total amount of dollars: {dollars}")
+def convert_conicoins():
+    # Amount of each currency equivalent to 1 conicoin
+    exchange_rates = {
+        "rub": 2.98,
+        "ars": 0.82,
+        "hnl": 0.17,
+        "aud": 1.9622,
+        "mad": 0.208
+    }
+    coins = float(input())
+    if coins.is_integer():
+        coins = int(coins)
+    for curr_type, rate in exchange_rates.items():
+        print(f"I will get {round((coins * rate),2)} {curr_type.upper()} from the sale of {coins} conicoins.")
 
 
-conicoins_to_dollars()
+convert_conicoins()
 
