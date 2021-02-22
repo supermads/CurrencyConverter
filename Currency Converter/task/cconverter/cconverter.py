d156c42039da85098415dcd19c9ae50a4a87b394
supermads <madisonsfahey@gmail.com>
2021-02-22 13:38:46 -0800

    Stage 3 of Project

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,9 +1,8 @@
 def conicoins_to_dollars():
-    conicoins = int(input())
-    dollars = conicoins * 100
-    print(f"I have {conicoins} conicoins.")
-    print(f"{conicoins} conicoins cost {dollars} dollars.")
-    print("I am rich! Yippee!")
+    conicoins = int(input("Please, enter the number of conicoins you have:\n"))
+    exchange_rate = float(input("Please, enter the exchange rate:\n"))
+    dollars = conicoins * exchange_rate
+    print(f"The total amount of dollars: {dollars}")
 
 
 conicoins_to_dollars()
